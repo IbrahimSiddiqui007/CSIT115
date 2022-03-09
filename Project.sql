1652d11a2ae6b04e075440e470ce3bdeec5e6115
IbrahimSiddiqui007 <87603789+IbrahimSiddiqui007@users.noreply.github.com>
2022-03-10 01:46:28 +0400

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -2,62 +2,80 @@ create database Project_system;
 use Project_system;
 
 create table SpecialFood(
-FName varchar(50),
+SFName varchar(50) unique Primary Key,
+Amnt varchar(30),
+FAmnt varchar(50),
+Diagnosis varchar(200),
+SInstructions varchar(100),
+Price int,
+foreign key (FDosage) references MedInformation(FDosage),
+foreign key (Diagnosis) references MedInformation(Diagnosis),
+foreign key (SInstructions) references MedInformation(SInstructions),
+foreign key (Price) references MedInformation(Price),
+foreign key (SFName) references MedInformation(MSFName)
+);
+
+create table medicines(
+MName varchar(50) unique Primary key,
 PDosage varchar(30),
 FDosage varchar(50),
 Diagnosis varchar(200),
 SInstructions varchar(100),
 Price int,
-foreign key (FDosage) references MedInformation(FDosage)
-);
-
-create table medicines(
-FName varchar(50),
-PDosage varchar(30),
-FDosage varchar(50),
-Diagnosis varchar(200),
-SInstructions varchar(100),
-Price int
+foreign key (FDosage) references MedInformation(FDosage),
+foreign key (Diagnosis) references MedInformation(Diagnosis),
+foreign key (SInstructions) references MedInformation(SInstructions),
+foreign key (Price) references MedInformation(Price),
+foreign key (MName) references MedInformation(MSFName)
 );
 
 create table MedInformation(
-PDosage varchar(30),
+CName varchar(100),
+PName varchar(100),
+MSFName varchar(50) unique,
 FDosage varchar(50),
 Diagnosis varchar(200),
 SInstructions varchar(100),
-Price int
+Price int,
+Primary key (CName, PName, MSFName),
+Foreign key (PName) references Pets(PName),
+Foreign key (CName) references Pets(CName)
 );
 
 create table Pets(
 CName varchar(100),
 PName varchar(100),
-CTPrimary int,
+CTPrimary int unique,
 PColor varchar(50),
 PSex enum("Male","Female"),
 DOB date,
 LWeight int,
 Breed varchar(50),
 Spcs varchar(100),
-foreign key (CName) references Customer(CName)
+primary key (CName,PName,CTPrimary),
+foreign key (CName) references Customer(CName),
+foreign key (CTPrimary) references Customer(TPrimary)
 );
 
 create table Customer(
 CName varchar(100),
 CAddress varchar(100),
-TPrimary int,
-SPrimary int,
+TPrimary int unique,
+SPrimary int unique,
 primary key (Cname, TPrimary)
 );
 
 create table Appointment(
 CName varchar(100),
 PName varchar(100),
-TPrimary int,
+TPrimary int unique,
 AppTime date,
 AppDate date,
 Pvisit varchar(50),
 Fee int,
-foreign key (CName) references Customer(CName)
+primary key (Cname, TPrimary),
+foreign key (CName) references Customer(CName),
+foreign key (TPrimary) references Customer(TPrimary)
 );
 
 create table Disease(
